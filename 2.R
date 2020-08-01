c2611b482aca2311a3db0be70446cd570c683424
yhaelJ <jyhael97@gmail.com>
2020-08-01 16:14:37 -0500

    Create 2.R
    plot 2 of assignment

---
+++ 2.R
@@ -0,0 +1,10 @@
+#2 
+
+dataFile <- "household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+png("figure2.png")
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
