d727e47aaa7412113df94d69667ba4801eae4239
yhaelJ <jyhael97@gmail.com>
2020-08-01 16:02:59 -0500

    Update code.R
    figure 3 changed

--- code.R
+++ code.R
@@ -56,15 +56,13 @@ subMetering1 <- as.numeric(subSetData$Sub_metering_1)
 subMetering2 <- as.numeric(subSetData$Sub_metering_2)
 subMetering3 <- as.numeric(subSetData$Sub_metering_3)
 
-
 png("figure4.png")
 par(mfrow = c(2, 2)) 
 
 plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power", cex=0.2)
-
 plot(datetime, voltage, type="l", xlab="datetime", ylab="Voltage")
-
 plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
+
 lines(datetime, subMetering2, type="l", col="red")
 lines(datetime, subMetering3, type="l", col="blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
